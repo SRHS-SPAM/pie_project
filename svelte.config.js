9e2a86c2676e0da1284672ac8990cf0c31de48be
andire120 <164429441+andire120@users.noreply.github.com>
2025-01-06 23:14:29 +0900

    j

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ export default {
       runtime: 'nodejs22.x', // Node.js 환경에서 실행 
       split: false, // 빌드 크기 최적화 비활성화
     }),
+    files:{
+      assets: 'public'
+    },
     paths: {
       // URL 경로를 커스터마이징하는 경우 설정 필요
       base: '', // 기본값: ''
