db2e85fe241fd49c69919206af8861c2f2ae24f4
JangSeung <0sunghee122@gmail.com>
2025-01-16 16:12:27 +0900

    .

--- src/routes/make_sheet2/ingredient/main_ingre/+page.svelte
+++ src/routes/make_sheet2/ingredient/main_ingre/+page.svelte
@@ -125,6 +125,17 @@
     
 </div>
 <style>
+    :global(html, body) {
+		margin: 0;
+		padding: 0;
+		height: 100%;
+		overflow: hidden;
+	}
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
 
     .main_box{
         width: 100vw;
